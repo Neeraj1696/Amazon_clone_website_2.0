9686c2b0ac9627e1aab929b8cd4b5c2e2665b57f
Neeraj Yadav <neeraj0yadav0@gmail.com>
2022-05-25 23:16:43 +0530

    initital commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
@@ -8,6 +9,13 @@ function Home() {
         className="home_image"
         src="https://store-images.s-microsoft.com/image/apps.16285.14618985536919905.552c0017-6644-49a8-8467-8f7b34ce0428.30ad6b05-16d9-4d5e-a242-43107708a16a?mode=scale&q=90&h=1080&w=1920"
         alt="Error_home_image"
+      />
+      <Product
+        id="123456"
+        title="Product Title"
+        price={11}
+        rating={5}
+        image="error_home_image"
       />
     </div>
   );
---
+++ src/components/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import"./Product.css"
+
+function Product(Props) {
+  return (
+    <div className="Product">
+      
+    </div>
+  )
+}
+
+export default Product
